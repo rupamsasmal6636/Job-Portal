552c13eb934bebeb4f68895f5e2db0bccb54dd8e
pooja <np249610@gmail.com>
2023-07-06 13:47:31 +0530

    adding login and signup page by pooja

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,13 +1,24 @@
 import './App.css';
 import JobDetails from './Pages/JobDetails/Components/JobDetails';
-import AdminPage from "./pages/Dashboard/Admin/Admin";
+import AdminPage from "./Pages/Dashboard/Admin/Admin";
+import HomePage from "./Pages/Signuplogindetails/Components/HomePage/HomePage";
+import Login from "./Pages/Signuplogindetails/Components/LoginPage/Login";
+import SignUpPage from "./Pages/Signuplogindetails/Components/SignUpPage/Signup";
+import { BrowserRouter, Routes,Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <AdminPage/>
-      <JobDetails/>
-
+      
+      <BrowserRouter>
+        <Routes>
+        <Route path='/' element ={<HomePage/>}/>
+        <Route path='/signup' element ={<SignUpPage/>}/>  
+        <Route path='/login' element ={<Login/>}/>
+        </Routes>
+      </BrowserRouter>
+      {/* <AdminPage/> */}
+      {/* <JobDetails/> */}
     </div>
   );
 }
