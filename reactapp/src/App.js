7405ada7e71031419d0f4b8464420f2d8d8f5498
Arivalagan-G <arivalagan269@gmail.com>
2023-07-07 18:48:31 +0530

    Landing Page added

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -2,18 +2,23 @@ import './App.css';
 import JobDetails from './Pages/JobDetails/Components/JobDetails';
 import AdminPage from "./Pages/Dashboard/Admin/Admin";
 import HomePage from "./Pages/Signuplogindetails/Components/HomePage/HomePage";
-import Login from "./Pages/Signuplogindetails/Components/LoginPage/Login";
+ import Login from "./Pages/Signuplogindetails/Components/LoginPage/Login";
 import SignUpPage from "./Pages/Signuplogindetails/Components/SignUpPage/Signup";
 import { BrowserRouter, Routes,Route } from "react-router-dom";
 import Navbar from './Pages/PostJobAndEditJob/components/Navbar';
-import Home from './Pages/PostJobAndEditJob/components/Home.js';
+  import Home from './Pages/PostJobAndEditJob/components/Home.js';
 import AddJob from './Pages/PostJobAndEditJob/jobs/AddJob.js';
 import EditJob from './Pages/PostJobAndEditJob/jobs/EditJob.js';
 import ViewJob from './Pages/PostJobAndEditJob/jobs/ViewJob.js';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
+import Home1 from "./Pages/LandingPage/Components/Home";
+import About from "./Pages/LandingPage/Components/About";
+ import Service from "./Pages/LandingPage/Components/Service";
+import Signup from "./Pages/LandingPage/Components/Signup";
+import Contact from "./Pages/LandingPage/Components/Contact";
+import Login1 from "./Pages/LandingPage/Components/Login";
 function App() {
   return (
     <div className="App">
@@ -25,10 +30,10 @@ function App() {
         <Route path='/login' element ={<Login/>}/>
         </Routes>
       </BrowserRouter> */}
-      {/* <AdminPage/> */}
-      {/* <JobDetails/> */}
+      {/* <AdminPage/> 
+       <JobDetails/> */}
 
-      <BrowserRouter>
+      {/* <BrowserRouter>
         <Navbar />
         <Routes>
           <Route path='/' element={<Home />}/>
@@ -37,7 +42,17 @@ function App() {
           <Route path='/viewjob/:id' element={<ViewJob />} />
         </Routes>
         <ToastContainer />
-      </BrowserRouter>
+      </BrowserRouter> */}
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home1 />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/service" element={<Service />} />
+        <Route path="/sign-up" element={<Signup />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login1 />} />
+      </Routes>
+    </BrowserRouter>
     </div>
   );
 }
