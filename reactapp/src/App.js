72c44f08eaf59479d496bf06654a19f1ca31d351
aravind2712 <aravindthangaraj7@gmail.com>
2023-07-09 22:17:31 +0530

    added file

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -2,11 +2,11 @@ import './App.css';
 import JobDetails from './Pages/JobDetails/Components/JobDetails';
 import AdminPage from "./Pages/Dashboard/Admin/Admin";
 import HomePage from "./Pages/Signuplogindetails/Components/HomePage/HomePage";
- import Login from "./Pages/Signuplogindetails/Components/LoginPage/Login";
+import Login from "./Pages/Signuplogindetails/Components/LoginPage/Login";
 import SignUpPage from "./Pages/Signuplogindetails/Components/SignUpPage/Signup";
 import { BrowserRouter, Routes,Route } from "react-router-dom";
 import Navbar from './Pages/PostJobAndEditJob/components/Navbar';
-  import Home from './Pages/PostJobAndEditJob/components/Home.js';
+import Home from './Pages/PostJobAndEditJob/components/Home.js';
 import AddJob from './Pages/PostJobAndEditJob/jobs/AddJob.js';
 import EditJob from './Pages/PostJobAndEditJob/jobs/EditJob.js';
 import ViewJob from './Pages/PostJobAndEditJob/jobs/ViewJob.js';
@@ -15,10 +15,12 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Home1 from "./Pages/LandingPage/Components/Home";
 import About from "./Pages/LandingPage/Components/About";
- import Service from "./Pages/LandingPage/Components/Service";
+import Service from "./Pages/LandingPage/Components/Service";
 import Signup from "./Pages/LandingPage/Components/Signup";
 import Contact from "./Pages/LandingPage/Components/Contact";
 import Login1 from "./Pages/LandingPage/Components/Login";
+import JobSearchPage from './Pages/JobSearch/JobSearchPage';
+
 function App() {
   return (
     <div className="App">
@@ -43,7 +45,8 @@ function App() {
         </Routes>
         <ToastContainer />
       </BrowserRouter> */}
-      <BrowserRouter>
+      
+      {/* <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home1 />} />
         <Route path="/about" element={<About />} />
@@ -52,7 +55,9 @@ function App() {
         <Route path="/contact" element={<Contact />} />
         <Route path="/login" element={<Login />} />
       </Routes>
-    </BrowserRouter>
+    </BrowserRouter> */}
+
+    <JobSearchPage />
     </div>
   );
 }
